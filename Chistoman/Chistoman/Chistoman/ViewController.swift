d387ab5624e7968f9d9f942c97c50dddd1c3f781
Bulat Galiev <bul-galiev@yandex.ru>
2018-03-01 00:31:34 +0300

    Added login viewcontroller

---
+++ Chistoman/Chistoman/Chistoman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chistoman
+//
+//  Created by Bulat Galiev on 28.02.2018.
+//  Copyright © 2018 Булат Галиев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
